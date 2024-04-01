19ada821d438214cf41d4bb6b70d2f7a338ee99b
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-04-02 02:43:52 +0600

    added updatet

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -1,28 +1,42 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import auth from "../../firebase.config";
 
-
 const Register = () => {
+  const [registationError, setRegistationError] = useState("");
+  const [success, setSussess] = useState("");
 
   const handelRegister = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
     console.log(email, password);
+    // password length check
+    if (password.length < 6) {
+      setRegistationError("Password should be at-list 6 carecter")
+      return;
+    }
 
-    createUserWithEmailAndPassword( auth ,email, password)
-      .then(result=> {
+
+
+    // reset error
+    setRegistationError("");
+    setSussess('');
+    // creat user
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
         console.log(result.user);
+        setSussess("user created successfully");
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
-    })
+        setRegistationError(error.message);
+      });
   };
 
   return (
     <div>
-      <h3 className="text-3xl">Please register hear</h3>
+      <h3 className="text-3xl font-bold">Please register hear</h3>
 
       <div className=" flex justify-center items-center border md:w-1/2 mx-auto ">
         <form onSubmit={handelRegister}>
@@ -30,14 +44,14 @@ const Register = () => {
             className="mb-4 mt-4 w-3/4 px-4 py-2"
             type="email"
             name="email"
-            placeholder="enter your email"
+            placeholder="enter your email" required
           ></input>
           <br />
           <input
             className="w-3/4 px-4 py-2 mb-4"
             type="password"
             name="password"
-            placeholder="enter your password"
+            placeholder="enter your password" required
           ></input>
           <br />
           <input
@@ -45,6 +59,14 @@ const Register = () => {
             type="submit"
             name="Register"
           ></input>
+
+          {
+            registationError && 
+            <p className="text-red-700">{registationError}</p>
+          }
+          {
+            success && <p className="text-green-700">{ success}</p>
+          }
         </form>
       </div>
     </div>
